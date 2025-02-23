bab4a211e91dc0da8c4c215360b847280e7755e6
sheshbzzarr <demerwdani@gmail.com>
2025-02-23 08:05:51 -0500

    update

--- Codeforces Round 161 (Div. 2)/A_Beautiful_Matrix.py
+++ Codeforces Round 161 (Div. 2)/A_Beautiful_Matrix.py
@@ -1,18 +1,18 @@
-matrix =[list(map(int,input().split())) for _ in range(5)]
-
-# find postion of '1'
-one_row ,one_col=-1,-1
-for i in range(5):
-    for j in range(5):
-        if matrix[i][j]==1:
-            one_row,one_col=i,j
+matrix = [list(map(int,input().split())) for _ in range(5)]
+one_row =-1
+one_col=-1
+for r in range(5):
+    for c in range(5):
+        if matrix[r][c]==1:
+            one_row=r 
+            one_col=c 
             break 
+    # this is to exit early it will be true becuase one_row=1 and finding one is guaranted thing 
     if one_row!=-1:
         break 
-center_row,center_col=2,2
-row_moves =abs(one_row-center_row)
-col_moves=abs(one_col-center_col)
-
-total_moves=row_moves+col_moves 
-
+row_center=2
+col_center =2 
+row_moves=abs(row_center-one_row)
+col_moves=abs(col_center-one_col)
+total_moves=row_moves+col_moves
 print(total_moves)
