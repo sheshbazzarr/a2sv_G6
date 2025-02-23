67f7ba36d70ab656d3cebdfe8ddae2f288e9d1b7
sheshbzzarr <demerwdani@gmail.com>
2025-02-23 08:06:44 -0500

    update

--- Codeforces Round 161 (Div. 2)/A_Beautiful_Matrix.py
+++ Codeforces Round 161 (Div. 2)/A_Beautiful_Matrix.py
@@ -1,14 +1,16 @@
 matrix = [list(map(int,input().split())) for _ in range(5)]
-one_row =-1
-one_col=-1
+one_row =-2
+one_col=-2
+found=False
 for r in range(5):
     for c in range(5):
         if matrix[r][c]==1:
             one_row=r 
             one_col=c 
+            found=True
             break 
     # this is to exit early it will be true becuase one_row=1 and finding one is guaranted thing 
-    if one_row!=-1:
+    if found:
         break 
 row_center=2
 col_center =2 
